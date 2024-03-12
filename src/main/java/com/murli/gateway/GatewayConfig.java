38d55261feec897fb19d1d24db0c43de2f61da09
Murli.Mohan <murli.mohan@talentica.com>
2024-03-12 18:25:06 +0530

    Initial commit

---
+++ src/main/java/com/murli/gateway/GatewayConfig.java
@@ -0,0 +1,22 @@
+package com.murli.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class GatewayConfig {
+
+  @Bean
+  public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    return builder.routes()
+        .route("product-service", r -> r.path("/api/products/**")
+            .uri("lb://product-service"))
+        .route("order-service", r -> r.path("/api/orders/**")
+            .uri("lb://order-service"))
+        .build();
+  }
+
+}
